0a53cf5b0e573a99cd89434707e384ce10427f33
zogmwa <zogmwatech@gmail.com>
2023-08-09 07:16:15 +0200

    plus React-bootstrap css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom';
+
 
 
 // axios.defaults.baseURL = 'https://0.0.0.0:5005'
@@ -17,11 +18,13 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter>
 
- 
+        <App />
+
+  </BrowserRouter>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
